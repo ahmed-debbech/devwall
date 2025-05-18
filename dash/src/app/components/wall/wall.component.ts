bd894551ec1d985bcf5db657cd3c3914065d780d
ahmed-debbech <debbech.ahmed@gmail.com>
2025-05-18 22:58:37 +0100

    add scrolling

--- dash/src/app/components/wall/wall.component.ts
+++ dash/src/app/components/wall/wall.component.ts
@@ -2,9 +2,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Post } from 'src/app/model/Post';
 import * as moment from 'moment';
 import { PostService } from 'src/app/services/post/post.service';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-wall',
+  standalone: true,
+  imports: [InfiniteScrollDirective],
   templateUrl: './wall.component.html',
   styleUrls: ['./wall.component.css']
 })
@@ -14,9 +17,7 @@ export class WallComponent {
   page_number : number = 0;
   posts : Post[] = [];
   
-  @ViewChild('scrollablePosts', { static: false }) 
-  public scrollablePosts !: ElementRef;
-  
+
   constructor(private postService : PostService){}
 
   ngOnInit(){
@@ -26,13 +27,6 @@ export class WallComponent {
   }
 
   onScrollLoadData() {
-    const nativeElement= this.scrollablePosts.nativeElement
-    console.log(this.scrollablePosts)
-    if(nativeElement.clientHeight + Math.round(nativeElement.scrollTop) === nativeElement.scrollHeight){
-      this.page_number++;
-      this.postService.getAllPaginatedPosts(this.page_number).subscribe((res) => {
-        this.posts = res
-      })
-    }
+
   }
 }
